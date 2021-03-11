1187509686d92ce8df25b1f0960555fa5ff04aaf
Vanessa G <vlopez_jv@hotmail.com>
2021-03-10 22:49:43 -0800

    added Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //import logo from './logo.svg';
+import Nav from './components/Nav';
 import './App.css';
 import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
