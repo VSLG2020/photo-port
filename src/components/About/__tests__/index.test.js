908dbef651b6e37bd27cfe499d464fcaf2a0c242
Vanessa G <vlopez_jv@hotmail.com>
2021-03-11 18:44:43 -0800

    added emoji test to nav

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import About from '..';
-
 import {render, cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
@@ -11,20 +9,13 @@ describe('About component', () => {
     // First Test The first test will be the baseline to verify that the component is rendering.
   it('renders', () => {
     render(<About />);
-
-    
   });
   
     // Second Test
-
     it('matches snapshot DOM node structure', () => {
         // render About
         const { asFragment } = render(<About />);
 
         expect(asFragment()).toMatchSnapshot();
-      
     });
-    
   })
-
-  
